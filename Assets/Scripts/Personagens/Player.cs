6f2ece9dfc9a5bb4eba58d8f4326973071622b2f
Luan <luanfrota@gmail.com>
2019-01-26 09:59:17 -0300

    agora vai

---
+++ Assets/Scripts/Personagens/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public partial class Player : MonoBehaviour
+{
+    public int playerNumber;
+    public bool isBusy;
+    public int busyTimer;
+    public Image busyClock;
+    public float hunger = 100*30;
+    public float higiene = 100*30;
+    public float tired = 100*30;
+    public float social = 100*30;
+    public float enjoy = 100*30;
+    
+    public float hungerRate;
+    public float higieneRate;
+    public float tiredRate;
+    public float socialRate;
+    public float enjoyRate;
+
+    public void eat(){
+        if(isBusy) return;
+        this.hunger = 100;
+        isBusy = true;
+        busyTimer = 20*30;
+        startTimer(busyTimer);
+    }
+    public void sleep(){
+        if(isBusy) return;
+        this.tired = 100;
+        isBusy = true;
+        busyTimer = 30*30;
+        startTimer(busyTimer);
+    }
+
+    public void clean(){
+        if(isBusy) return;
+        this.higiene = 100;
+        isBusy = true;
+        busyTimer = 40*30;
+        startTimer(busyTimer);
+    }
+    public void socialize(){
+        if(isBusy) return;
+        this.social = 100;
+        isBusy = true;
+        busyTimer = 50*30;
+        startTimer(busyTimer);
+    }
+
+    public void entertain(){
+        if(isBusy) return;
+        this.enjoy = 100;
+        isBusy = true;
+        busyTimer = 60*30;
+        startTimer(busyTimer);
+    }
+
+    void FixedUpdate(){
+        if(busyTimer>0){
+            busyTimer--;
+        }else{
+            busyTimer = 0;
+            isBusy = false;
+            consumeResources();
+        }
+    }
+
+    public void consumeResources(){
+        if(hunger>0)
+        hunger -= hungerRate;
+        if(higiene>0)
+        higiene -= higieneRate;
+        if(tired>0)
+        tired -= tiredRate;
+        if(enjoy>0)
+        enjoy -= enjoyRate;
+        if(social>0)
+        social -= socialRate;
+    }
+
+
+    public void startTimer(int time){
+        busyClock.gameObject.SetActive(true);
+        busyClock.gameObject.GetComponent<ClockHandler>().maxTime = time;
+        busyClock.gameObject.GetComponent<ClockHandler>().curTime = time;
+    }
+}
---
+++ Assets/Scripts/UI/BarHandling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarHandling : MonoBehaviour
+{
+    public Player associatedPlayer;
+    public int barType;
+
+    void FixedUpdate(){
+        if (barType ==0)
+        this.gameObject.GetComponent<Image>().fillAmount = associatedPlayer.hunger*30/3000;
+        else if (barType ==1)
+        this.gameObject.GetComponent<Image>().fillAmount = associatedPlayer.higiene*30/3000;
+        else if (barType ==2)
+        this.gameObject.GetComponent<Image>().fillAmount = associatedPlayer.tired*30/3000;
+        else if (barType ==3)
+        this.gameObject.GetComponent<Image>().fillAmount = associatedPlayer.enjoy*30/3000;
+        else if (barType ==4)
+        this.gameObject.GetComponent<Image>().fillAmount = associatedPlayer.social*30/3000;
+    }
+}
